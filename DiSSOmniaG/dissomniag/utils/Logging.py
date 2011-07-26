543d932d347b69ee1ca96ad10f087980b99d8b1d
Sebastian Wallat <sebastian@wallat.de>
2011-07-26 21:36:00 +0200

    Some utils added

---
+++ DiSSOmniaG/dissomniag/utils/Logging.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 26.07.2011
+
+@author: Sebastian Wallat
+"""
+import logging, os
+import logging.handlers
+import time
+import glob
+
+import dissomniag.config
+
+def setUpLogger(logger):
+    logger.setLevel(logging.DEBUG)
+    if not os.path.exists(dissomniag.config.log.logDir):
+        os.makedirs(dissomniag.config.log.logDir)
+    
+    debugFile = dissomniag.config.log.debugFilename
+    warningFile = dissomniag.config.log.warningFilename
+    
+    
+    
+    if not dissomniag.config.log.logDir.endswith("/"):
+        debugFile = "" + dissomniag.config.log.logDir + "/" + debugFile
+        warningFile = "" + dissomniag.config.log.logDir + "/" + warningFile
+    else:
+        debugFile = "" + dissomniag.config.log.logDir + debugFile
+        warningFile = "" + dissomniag.config.log.logDir + warningFile
+        
+    firstRun = True
+    if os.path.isfile(debugFile) or os.path.isfile(warningFile):
+        firstRun = False
+
+    debugHandler = logging.handlers.RotatingFileHandler(debugFile,
+                                                    mode = 'a',
+                                                    maxBytes = 1000000,
+                                                    backupCount = 10)
+    debugHandler.setLevel(logging.DEBUG)
+    
+    warningHandler = logging.handlers.RotatingFileHandler(warningFile,
+                                                          mode = 'a',
+                                                          maxBytes = 1000000,
+                                                          backupCount = 10)
+    warningHandler.setLevel(logging.WARNING)
+    formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+    debugHandler.setFormatter(formatter)
+    warningHandler.setFormatter(formatter)
+    logger.addHandler(warningHandler)
+    logger.addHandler(debugHandler)
+    
+    
+    if not firstRun:
+        # Add timestamp
+        logger.warning('\n---------\nLog closed on %s.\n---------\n' % time.asctime())
+        
+        # Roll over on application start
+        for handler in logger.handlers:
+            handler.doRollover()
+    
+    # Add timestamp
+    logger.warning('\n---------\nLog started on %s.\n---------\n' % time.asctime())
+    
+    return logger
+
+logger = setUpLogger(logging.getLogger(''))
