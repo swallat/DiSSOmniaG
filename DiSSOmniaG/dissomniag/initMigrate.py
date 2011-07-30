e23a4bc12a6c38da50615389971ec9d517826662
Sebastian Wallat <sebastian.wallat@uni-due.de>
2011-07-30 15:42:44 +0200

    Conformance update migrate.versioning.exceptions as mExceptions

--- DiSSOmniaG/dissomniag/initMigrate.py
+++ DiSSOmniaG/dissomniag/initMigrate.py
@@ -5,7 +5,11 @@ Created on 27.07.2011
 @author: Sebastian Wallat
 """
 import migrate.versioning.api
-import migrate.versioning.exceptions
+try:
+    import migrate.versioning.exceptions as mException
+except ImportError:
+    import migrate.exceptions as mException
+    
 import dissomniag.config as config
 
 def init():
@@ -14,7 +18,7 @@ def init():
     """
     try:
         migrate.versioning.api.version_control(config.db.db_string, config.db.migrate_repository)
-    except migrate.versioning.exceptions.DatabaseAlreadyControlledError:
+    except mException.DatabaseAlreadyControlledError:
         print("DB under version control.")
       
     migrate.versioning.api.upgrade(config.db.db_string, config.db.migrate_repository)
