4315181daacc2e540079ed9032945b019f17a229
Sebastian Wallat <sebastian.wallat@uni-due.de>
2011-11-17 16:40:19 +0100

    Minor changes

---
+++ DiSSOmniaG/dissomniag/GitEnvironment.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 16.11.2011
+
+@author: Sebastian Wallat
+'''
+import dissomniag
+import logging
+
+log = logging.getLogger("GitEnvironment")
+
+
+class GitEnvironment(dissomniag.utils.SingletonMixin):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+    
+    
+    def prepare(self):
+        pass
+    
+    def check(self):
+        pass
+    
---
+++ DiSSOmniaG/dissomniag/cliApi/ManageApps.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 17.11.2011
+
+@author: Sebastian Wallat
+'''
+import logging, argparse
+from colorama import Fore, Style, Back
+import sys, time
+import getpass
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
+import dissomniag
+from dissomniag.utils import CliMethodABCClass
+from dissomniag import taskManager
+
+log = logging.getLogger("cliApi.ManageApps")
+
+class apps(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal#
+        
+class addApp(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class delApp(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class addAppUser(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class delAppUser(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class addAppVm(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class delAppVm(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class appUpdate(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class appCompile(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class appStop(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
+        
+class appStart(CliMethodABCClass.CliMethodABCClass):
+    
+    def implementation(self, *args):
+        sys.stdout = self.terminal
+        sys.stderr = self.terminal
