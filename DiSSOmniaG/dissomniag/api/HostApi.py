66534c6b7138caedab13271c2592c24cb8f0a750
Sebastian Wallat <sebastian@wallat.de>
2012-07-18 15:47:08 +0200

    host api changed

--- DiSSOmniaG/dissomniag/api/HostApi.py
+++ DiSSOmniaG/dissomniag/api/HostApi.py
@@ -15,28 +15,28 @@ log = logging.getLogger("api.Hosts.py")
 
 def getHostList(user):
     return "HALLO"
-    root = etree.Element("result")
-    errorMsg = etree.SubElement(root, "error")
+    #root = etree.Element("result")
+    #errorMsg = etree.SubElement(root, "error")
     
-    dissomniagPublicSshKey = etree.SubElement(root, "public-ssh-key")
+    #dissomniagPublicSshKey = etree.SubElement(root, "public-ssh-key")
     
-    text = ""
-    with open(os.path.abspath(dissomniag.config.dissomniag.rsaKeyPublic), "r") as f:
-        text = f.read();
+    #text = ""
+    #with open(os.path.abspath(dissomniag.config.dissomniag.rsaKeyPublic), "r") as f:
+    #    text = f.read();
     
-    dissomniagPublicSshKey.text = text
-    hostList = etree.SubElement(root, "host-list")
+    #dissomniagPublicSshKey.text = text
+    #hostList = etree.SubElement(root, "host-list")
     
-    session = dissomniag.Session()
+    #session = dissomniag.Session()
     
-    try:
-        hosts = session.query(dissomniag.model.Host).all()
-    except NoResultFound:
-        pass
-    else:
-        for host in hosts:
-            hostList.append(host.getUserXml())
+    #try:
+    #    hosts = session.query(dissomniag.model.Host).all()
+    #except NoResultFound:
+    #    pass
+    #else:
+    #    for host in hosts:
+    #        hostList.append(host.getUserXml())
             
-    retString = etree.tostring(root, pretty_print = True)
-    log.info("Send hostList: " + retString)
-    return retString
+    #retString = etree.tostring(root, pretty_print = True)
+    #log.info("Send hostList: " + retString)
+    #return retString
